02e76147273aa1edfeab2332f23c8b9074105dc2
João Victor Moreira <joaovictorfersuramoreira@gmail.com>
2022-11-07 22:44:07 -0300

    SkillsControls.cs: optimized code.

--- Assets/StrategyPattern/Scripts/Context/SkillsControls.cs
+++ Assets/StrategyPattern/Scripts/Context/SkillsControls.cs
@@ -2,22 +2,26 @@ using UnityEngine;
 
 public class SkillsControls : MonoBehaviour
 {
-    private ISkill _currentSkill = new Heal();
+    private ISkill _currentSkill;
     
     private void Update()
     {
         if (Input.GetButtonDown("Fire1")) // M1
         {
-            _currentSkill.Use();
-        }
-        if (Input.GetKey("1")) // 1
-        {
+            Debug.Log("Switched to Heal.");
             _currentSkill = new Heal();
         }
-        if (Input.GetKey("2")) // 2
+        if (Input.GetButtonDown("Fire2")) // M2
         {
+            Debug.Log("Switched to Provoke.");
             _currentSkill = new Provoke();
         }
+        if (Input.GetButtonDown("Fire3")) // M3
+        {
+            if (_currentSkill != null)
+            {
+                _currentSkill.Use();
+            }
+        }
     }
-
 }
